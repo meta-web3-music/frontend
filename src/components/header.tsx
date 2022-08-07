23095b59a377769e56f06d1795f4e741cc1d7249
Mike Starr <imhs@protonmail.com>
2022-08-07 14:40:34 -0400

    Linked logo to home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,8 +21,10 @@ function Header() {
     <>
       <header className="flex flex-row items-center justify-between md:px-10 mb-10 py-2 bg-slate-50 border-b">
         {/* headline */}
-        <div className="">
-          <h1 className="text-3xl font-bold m-3">OCTAV3</h1>
+        <div className="" onClick={navigateToHottestSongPage}>
+          <h1 className="text-3xl font-bold m-3 hover:cursor-pointer">
+            OCTAV3
+          </h1>
         </div>
         {/* Ad marketplace */}
         <div className="flex flex-col justify-center">
