0da21090286855aea7102e7d6683b821300c057b
thisisommore <ommore501@gmail.com>
2022-08-07 03:23:51 +0000

    Add miniting code

--- src/components/HottestSongs.tsx
+++ src/components/HottestSongs.tsx
@@ -6,7 +6,20 @@ import { useQuery } from "@apollo/client";
 import { GET_ALL_MUSIC } from "../graph-ql/queries/GET_ALL_MUSIC/getAllMusic";
 import { GetAllMusic } from "../graph-ql/queries/GET_ALL_MUSIC/__generated__/GetAllMusic";
 import { NFTStorage, File } from "nft.storage";
-
+import { MusicNftMetaData } from "../types/MusicNFTData";
+import {
+  MusicNFT__factory,
+  ZoraAsk__factory,
+  ZoraModuleManager__factory,
+} from "../contracts";
+import {
+  AdvNFTAddr,
+  MusicNFTAddr,
+  ZoraAskAddr,
+  ZoraModuleManagerAddr,
+} from "../env";
+import { AdvNftMetaData } from "../types/AdvNFTData";
+import { BigNumber } from "ethers";
 const client = new NFTStorage({
   token: process.env.NEXT_PUBLIC_NFT_STORAGE_TOKEN ?? "",
 });
@@ -27,10 +40,82 @@ const HottestSongs: React.FC = () => {
     // query IPFS and store music
     // take back returned music CID
     // create an object payload, stringify and pass as argument to contract function
-    const artifactHash = await client.storeBlob(
+    const musicAssetHash = await client.storeBlob(
       formData.upload[0].originFileObj
     );
-    console.log(artifactHash);
+    console.log(musicAssetHash);
+    const metaDataObj: MusicNftMetaData = {
+      body: {
+        artist: "Artist",
+        artwork: {
+          info: {
+            mimeType: "image/jpeg",
+            uri: "cover image uri",
+          },
+          isNft: false,
+          nft: null,
+        },
+        duration: 100,
+        mimeType: "audio/mp3",
+        notes: "",
+        project: null,
+        title: "song title",
+        trackNumber: "",
+        version: "",
+        visualizer: "",
+      },
+    };
+
+    const musicMetadataHash = await client.storeBlob(
+      new Blob([JSON.stringify(metaDataObj)])
+    );
+
+    const advNftDataObj: AdvNftMetaData = {
+      description: "Adv nft for music xyz",
+      mimeType: "image/jpeg",
+      name: "ADV NFT",
+      version: "",
+    };
+
+    const advNftMetaDataHash = await client.storeBlob(
+      new Blob([JSON.stringify(advNftDataObj)])
+    );
+
+    const musicNft = MusicNFT__factory.connect(MusicNFTAddr, signer);
+    const resCreateMusicWithAdv = await musicNft
+      .createMusicWithAdv(
+        musicMetadataHash,
+        musicAssetHash,
+        advNftMetaDataHash,
+        // TODO: generate this, maybe not important for mvp
+        "advAssetHash",
+        1000
+      )
+      .then((e) => e.wait());
+    const advNftID = resCreateMusicWithAdv.events?.[3].args
+      ?.tokenId as BigNumber;
+
+    const zoraModuleManager = ZoraModuleManager__factory.connect(
+      ZoraModuleManagerAddr,
+      signer
+    );
+
+    const isModuleApproved = await zoraModuleManager.isModuleApproved(
+      "user addr",
+      ZoraAskAddr
+    );
+    if (!isModuleApproved) {
+      await zoraModuleManager.setApprovalForModule(ZoraAskAddr, true);
+    }
+    const zoraNft = ZoraAsk__factory.connect(ZoraAskAddr, signer);
+    await zoraNft.createAsk(
+      AdvNFTAddr,
+      advNftID,
+      123,
+      "0x0000000000000000000000000000000000000000",
+      "todo: user addr",
+      ""
+    );
 
     // close modal
     handleModal();
