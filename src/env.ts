e61682fdaeb012840606300a8b24d2e3cac29c8d
thisisommore <ommore501@gmail.com>
2022-08-24 18:41:24 +0000

    Update contract addresses

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-export const AdvNFTAddr = "0xEDeb296201255F4CD25cad06bede20D1902921fB";
-export const MusicNFTAddr = "0xC715aEeD45eE6fE9e712F149aCc010b7A7EC4FBd";
+export const AdvNFTAddr = "0x13C2a78c4F1a14AeC47b6711130583957F405BdB";
+export const MusicNFTAddr = "0xa5ff6bc6BC1586aaFD675e0D0469Da954D9d76b6";
 export const ZoraAskAddr = "0xCe6cEf2A9028e1C3B21647ae3B4251038109f42a";
 export const ZoraModuleManagerAddr = "0x850A7c6fE2CF48eea1393554C8A3bA23f20CC401";
 export const GraphQLEndpoint = "https://api.thegraph.com/subgraphs/name/thisisommore/muzik"
