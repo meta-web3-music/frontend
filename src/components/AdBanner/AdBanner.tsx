63b4622f23686cd03d086aaa647bb571783625a8
mujeex <mujahid.bappai@yahoo.com>
2022-08-28 14:12:53 +0100

    fix: remove banner if song has no adv NFT

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -8,6 +8,8 @@ import styles from './AdBanner'
 import { AdBannerProps } from "./AdBanner.types";
 
 const AdBanner: React.FC<AdBannerProps> = ({ imageUrl }) => {
+
+  if(!imageUrl) return <div></div>
   return (
     <div style={{marginBottom:'1em'}}>
       <Image width={"100%"} height={"200px"}   src={imageUrl} />
