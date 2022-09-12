7598651172338f41a7e52fda5fcd3dce145093b4
mujeex <mujahid.bappai@yahoo.com>
2022-09-12 11:50:56 +0100

    refactor: move adList to own file

--- pages/adMarketPlace/index.tsx
+++ pages/adMarketPlace/index.tsx
@@ -1,34 +1,32 @@
-import { useContext } from "react";
-import Header from "../../src/components/Header/header";
+import { useState } from "react";
 
 // antd imports
-import { DownOutlined } from "@ant-design/icons";
-import { Button, Dropdown, Menu, Space, Radio, Typography, List } from "antd";
-import type { RadioChangeEvent } from "antd";
+import { Typography} from "antd";
+const { Title, Text } = Typography;
 
 import {
-  GetAllAsks,
   GetAllAsks_asks,
 } from "../../src/graph-ql/queries/GET_ALL_ASKS/__generated__/GetAllAsks";
-import { GET_ALL_ASKS } from "../../src/graph-ql/queries/GET_ALL_ASKS/getAllAsks";
-import { useQuery } from "@apollo/client";
+
 import { AdvNFTAddr, ZoraAskAddr } from "../../src/env";
-import { useEffect, useState } from "react";
 import { AdvNftMetaData } from "../../src/types/AdvNFTData";
 
 // custom-component imports
 import AdModal from "../../src/components/AdModal/AdModal";
+import Header from "../../src/components/Header/header";
+import Adlist from "../../src/components/MarketPlacePage/Adlist/Adlist";
+
+// wagmi imports
+import {useSigner} from 'wagmi'
 
 import { NFTStorage, File } from "nft.storage";
 
 import {
   AdvNFT__factory,
   ZoraAsk__factory,
-  ZoraModuleManager__factory,
 } from "../../src/contracts";
-import { fetchIpfs } from "../../src/services/ipfs/fetchIpfs";
 
-const { Title, Text } = Typography;
+
 
 // create client instance for nft.storage
 const client = new NFTStorage({
@@ -41,6 +39,7 @@ const AdMarketPlace: React.FC = () => {
   const [selectedAdv, setSelectedAdv] = useState<GetAllAsks_asks>();
   const [showModal, setShowModal] = useState<boolean>(false);
   const [isCreatingAd, setIsCreatingAd] = useState<boolean>(false);
+  const {data: signer} = useSigner()
 
   const handleAdModal = () => {
     setShowModal(!showModal);
@@ -66,14 +65,13 @@ const AdMarketPlace: React.FC = () => {
       const metadataHash = await client.storeBlob(
         new Blob([JSON.stringify(advNftDataObj)])
       );
-      const signer = (await walletContext.getWeb3Provider()).getSigner();
+
       const adNft = AdvNFT__factory.connect(AdvNFTAddr, signer);
       const zoraAsks = ZoraAsk__factory.connect(ZoraAskAddr, signer);
       const zeroAddr = "0x0000000000000000000000000000000000000000";
       console.log(selectedAdv);
       if (!selectedAdv?.token.id) {
         throw new Error("Failed to get selected adv id");
-        return;
       }
       console.log("handleAdForm: Filling Zora Asks");
 
@@ -113,6 +111,7 @@ const AdMarketPlace: React.FC = () => {
 
     setShowModal(true);
   };
+  
   return (
     <>
       <Header />
@@ -131,93 +130,5 @@ const AdMarketPlace: React.FC = () => {
   );
 };
 
-interface AdlistProp {
-  onHandleModal: () => void;
-  onRentClick: (advNft: GetAllAsks_asks) => void;
-}
-
-const Adlist: React.FC<AdlistProp> = ({ onRentClick }) => {
-  const {
-    loading: isLoadingAllAsks,
-    data: allAsksConnection,
-    error: allAskError,
-  } = useQuery<GetAllAsks>(GET_ALL_ASKS, {
-    variables: {
-      nftContractAddr: AdvNFTAddr.toLowerCase(),
-    },
-  });
-
-
-  const {getWeb3Provider,web3Provider} = useContext(WalletContext)
-
-  
-
-  return (
-    <>
-
-    <List
-      loading={isLoadingAllAsks}
-      style={{
-        width: "700px",
-        alignSelf: "center",
-        border: "1px solid #e5e5e5",
-        borderRadius: "20px",
-        padding: "1em",
-      }}
-      itemLayout="horizontal"
-      dataSource={allAsksConnection?.asks}
-      renderItem={(item) => {
-        return (
-          <List.Item
-          extra={
-            <Button onClick={web3Provider === undefined ? getWeb3Provider : () => onRentClick(item)}>Rent Ad Space</Button>
-          }
-          >
-            <List.Item.Meta
-              // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title={<TitleNode item={item} />}
-              description="TODO: fetch desc from ipfs"
-            />
-          </List.Item>
-        );
-      }}
-    />
-      </>
-  );
-};
-
-interface TitleProps {
-  item: GetAllAsks_asks;
-}
-
-const TitleNode: React.FC<TitleProps> = ({ item }) => {
-  const [metaData, setMetaData] = useState<AdvNftMetaData>();
-
-  const fetchMetaData = async () => {
-    const advMetaData = await fetchIpfs<AdvNftMetaData>(
-      item.token.metaDataHash
-    );
-    setMetaData(advMetaData);
-  };
-  useEffect(() => {
-    fetchMetaData();
-  }, [item]);
-  return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <Title style={{ marginRight: "5px", marginBottom: "0px" }} level={5}>
-        {metaData?.name}
-      </Title>
-      <span
-        style={{
-          background: "#f4f4f4",
-          padding: "2px 6px",
-          borderRadius: "20px",
-        }}
-      >
-        {item.token.id}
-      </span>
-    </div>
-  );
-};
 
 export default AdMarketPlace;
