a3699b3fa8a2b9d1c9ec389743d237db22dff869
Om More <51229945+thisisommore@users.noreply.github.com>
2022-09-18 18:49:37 +0530

    Display art work with music details (#95)

--- pages/HottestSongs.tsx
+++ pages/HottestSongs.tsx
@@ -33,7 +33,7 @@ const HottestSongs: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col align-center justify-center w-full md:w-4/5 lg:w-2/3 m-2 md:m-auto px-2 text-left">
+    <div className="flex flex-col align-center justify-center w-full md:w-5/6 m-2 md:m-auto px-2 text-left">
       <Title level={3}>Hottest Songs</Title>
       <Text>Place your ads under the hottest songs</Text>
       <MintSong />
