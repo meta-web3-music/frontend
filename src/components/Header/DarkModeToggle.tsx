7141fd473e8fe799c41c6d350c09291abf169288
thisisommore <ommore501@gmail.com>
2022-09-19 19:53:47 +0000

    Fix toggle which brokes after second visit to page

--- src/components/Header/DarkModeToggle.tsx
+++ src/components/Header/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import styled from "styled-components";
 
@@ -8,16 +8,11 @@ const Styled = styled.div`
   }
 `;
 function DarkModeToggle() {
-  const { theme, setTheme } = useTheme();
-
-  if (theme == "system") {
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (isDark) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  }
+  const { resolvedTheme, setTheme } = useTheme();
+  const [_theme, _setTheme] = useState<string>();
+  useEffect(() => {
+    _setTheme(resolvedTheme);
+  }, [resolvedTheme]);
   return (
     <Styled className="ml-auto flex items-center relative">
       <div className="opacity-0 text-gray-700 text-3xl mr-5 pointer-events-none">
@@ -25,15 +20,15 @@ function DarkModeToggle() {
       </div>
       <div
         className={`text-gray-700 text-3xl mr-5 duration-300 absolute left-0 spring cursor-pointer ${
-          theme == "dark" ? "opacity-0 pointer-events-none rotate-180" : ""
+          _theme == "dark" ? "opacity-0 pointer-events-none rotate-180" : ""
         }`}
         onClick={() => setTheme("dark")}
       >
         <span className="iconify" data-icon="bi:sun-fill" />
       </div>
       <div
-        className={`text-gray-300 text-2xl mr-5 duration-300 absolute left-0 spring cursor-pointer ${
-          theme == "dark" ? "" : "opacity-0 pointer-events-none -rotate-180"
+        className={`text-gray-300 text-2xl mr-5 duration-300 absolute left-0 spring cursor-pointer translate-x-1 ${
+          _theme == "dark" ? "" : "opacity-0 pointer-events-none -rotate-180"
         }`}
         onClick={() => setTheme("light")}
       >
