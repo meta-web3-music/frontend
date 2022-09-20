3049c055dfc10b0ffb18565218c018716a6f3a2d
thisisommore <ommore501@gmail.com>
2022-09-20 13:27:49 +0000

    Update smart contract address

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const AdvNFTAddr = "0x0b6abA80D045a29d1c720284421094eB0EF2291D";
-export const MusicNFTAddr = "0x8A316d5f0b04eA495D3808Fb66144c90f2da662e";
-export const MarketPlaceAddr = "0x890351b827Bd1f48e479A93dd3397C6b16b33870";
+export const AdvNFTAddr = "0x972CddA647fC1c2D12505d968D906664C1D9205f";
+export const MusicNFTAddr = "0x1a8758092fe15983b9f4D6C5E0E0c7315F0b9239";
+export const MarketPlaceAddr = "0x652e32c2273a2D0b0c5494F30a794bE67Cc8b12D";
 export const GraphQLEndpoint = "https://query.graph.ommore.me/subgraphs/name/thisisommore/muzik"
