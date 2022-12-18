922483039173566602400310af524ebb7a37611b
thisisommore <ommore501@gmail.com>
2022-12-18 12:34:19 +0000

    Update smart contract address

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const AdvNFTAddr = "0xe8d5313592e7f284d7b87270e889c9651c3a7A89";
-export const MusicNFTAddr = "0xb06e737940b4212B9809e50Fe223Bd76bF13D99f";
-export const MarketPlaceAddr = "0xD2af673b477678EfECb8169309F6AaEAab290DF1";
+export const AdvNFTAddr = "0xED7F9873657dA47f92ef1d1E2BE3713De1e901cC";
+export const MusicNFTAddr = "0xd0851856D6EBbC3f13e69370A0e811f512933455";
+export const MarketPlaceAddr = "0xFA989709E33998fD00670c07672E40b078F7035A";
 export const GraphQLEndpoint = "https://api.thegraph.com/subgraphs/name/thisisommore/muzik"
