57e757ba3d46741f8a75a9de5cc34f833f1f0d7d
thisisommore <ommore501@gmail.com>
2023-07-06 09:19:16 +0000

    Use new API for fetching songs in home page

--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -5,19 +5,15 @@ import { useQuery } from "@apollo/client";
 import { SongListProps } from "./SongList.types";
 
 import SongListItemMuzik from "./SongListItemMuzik";
-import { GET_ALL_MUSIC } from "@/graph-ql/queries/muzik/GET_ALL_MUSIC/getAllMusic";
+import { GET_ALL_MUSIC } from "@/graph-ql/queries/octav3/GET_ALL_MUSIC/getAllMusic";
 
 const SongList: React.FC<SongListProps> = ({ playSong }) => {
-  const { data: allMusicConnection } = useQuery(GET_ALL_MUSIC, {
-    variables: {
-      currentTime: Math.floor(Date.now() / 1000).toString(),
-    },
-  });
+  const { data: allMusicConnection } = useQuery(GET_ALL_MUSIC);
 
   return (
     <>
       <div className="flex flex-wrap justify-center pt-4 mb-4 items-start h-[70vh] overflow-y-scroll">
-        {allMusicConnection?.musicNFTs.map((e) => (
+        {allMusicConnection?.octaveTokens.map((e) => (
           <SongListItemMuzik
             key={e.id}
             musicNft={e}
--- src/types/MusicNFTData.ts
+++ src/types/MusicNFTData.ts
@@ -1,34 +1,42 @@
 export interface MusicNftMetaData {
-    body: Body
-    origin?: Origin
+    animation_url: string;
+    artist: string;
+    artwork: Artwork;
+    attributes: Attribute[];
+    bpm: null;
+    credits: null;
+    description: string;
+    duration: number;
+    external_url: string;
+    genre: string;
+    image: string;
+    isrc: null;
+    key: null;
+    license: string;
+    locationCreated: null;
+    losslessAudio: string;
+    lyrics: null;
+    mimeType: string;
+    nftSerialNumber: number;
+    name: string;
+    originalReleaseDate: null;
+    project: null;
+    publisher: null;
+    recordLabel: null;
+    tags: null;
+    title: string;
+    trackNumber: number;
+    version: string;
+    visualizer: null;
 }
 
-interface Body {
-    artist: string
-    artwork: Artwork
-    duration: number
-    mimeType: string
-    project: null
-    title: string
-    trackNumber: string
-    version: string
-    visualizer: string
+export type Artwork = {
+    mimeType: string;
+    uri: string;
+    nft: null;
 }
 
-interface Artwork {
-    info: Info
-    isNft: boolean
-    nft: null
-}
-
-interface Info {
-    mimeType: string
-    uri: string
-}
-
-interface Origin {
-    algorithm: string
-    encoding: string
-    signature: string
-    publicKey: string
+export type Attribute = {
+    trait_type?: string;
+    value: string;
 }
