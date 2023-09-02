a0e541bc1de4ae48d135f192fb2135b8c9cb534b
thisisommore <ommore501@gmail.com>
2023-09-02 15:35:46 +0000

    OUpload: extract className from props

--- src/components/OUpload/OUpload.tsx
+++ src/components/OUpload/OUpload.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import OButton from "../OButton/OButton";
 
 type Props = {
   buttonText: string;
@@ -10,50 +9,65 @@ type Props = {
 const OUpload = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement> & Props
->(({ buttonText, className, label, info, ...p }, ref) => {
-  const fileRef = useRef<HTMLInputElement | null>(null);
-  const [fileName, setFileName] = useState<string>();
+>(
+  (
+    {
+      buttonText,
+      //TODO, this was used by some components to add styles for old upload button, now we have new ui, not sure how it will look on new ui
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      className,
+      label,
+      info,
+      ...p
+    },
+    ref
+  ) => {
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const [fileName, setFileName] = useState<string>();
 
-  useEffect(() => {
-    fileRef.current?.addEventListener("change", (f) =>
-      setFileName((f.target as HTMLInputElement)?.files?.[0].name)
-    );
-  }, []);
-  return (
-    <div>
-      <label
-        className="text-[#000000c9] dark:text-white text-lg"
-        htmlFor={p.name}
-      >
-        {label}
-      </label>
-      <div
-        className="w-full h-28 border-2 border-dashed flex justify-center items-center rounded-lg text-[#BFB00D] z-10 mt-1"
-        onClick={() => {
-          fileRef.current?.click();
-        }}
-      >
-        <p>{fileName ? fileName : buttonText}</p>
+    useEffect(() => {
+      fileRef.current?.addEventListener("change", (f) =>
+        setFileName((f.target as HTMLInputElement)?.files?.[0].name)
+      );
+    }, []);
+    return (
+      <div>
+        <label
+          className="text-[#000000c9] dark:text-white text-lg"
+          htmlFor={p.name}
+        >
+          {label}
+        </label>
+        <div
+          className="w-full h-28 border-2 border-dashed flex justify-center items-center rounded-lg text-[#BFB00D] z-10 mt-1"
+          onClick={() => {
+            fileRef.current?.click();
+          }}
+        >
+          <p>{fileName ? fileName : buttonText}</p>
+        </div>
+
+        {info && (
+          <p className="text-[#0000007c] dark:text-white mt-2">{info}</p>
+        )}
+        <input
+          ref={(node) => {
+            fileRef.current = node;
+            if (typeof ref === "function") {
+              ref(node);
+            } else if (ref) {
+              ref.current = node;
+            }
+          }}
+          type="file"
+          className="opacity-0 -translate-y-3/4 -z-10 relative"
+          {...p}
+        />
+        <div className="-mt-5"></div>
       </div>
-
-      {info && <p className="text-[#0000007c] dark:text-white mt-2">{info}</p>}
-      <input
-        ref={(node) => {
-          fileRef.current = node;
-          if (typeof ref === "function") {
-            ref(node);
-          } else if (ref) {
-            ref.current = node;
-          }
-        }}
-        type="file"
-        className="opacity-0 -translate-y-3/4 -z-10 relative"
-        {...p}
-      />
-      <div className="-mt-5"></div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 OUpload.displayName = "OUpload";
 
